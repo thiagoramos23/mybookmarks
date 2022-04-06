c53ad351a6de7a7016008773c3ba1d2512493038
Thiago Ramos <thiagoramos.al@gmail.com>
2022-04-05 21:42:46 -0300

    Testing URL

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :mybookmarks, MybookmarksWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: System.get_env("DOMAIN_HTTP_SCHEME"), host: System.get_env("DOMAIN_HTTP_URL"), port: 80],
+  url: [scheme: "http", host: "bookmark.quaseninja.com", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
